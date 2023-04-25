56464b42652f567b9a038a920c98e7665a5f06f4
kaaly06 <kaliany.palma@etec.sp.gov.br>
2023-04-25 15:53:45 -0300

    atualização

---
+++ AppBancoDigital/AppBancoDigital/Model/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public string tipo { get; set; }
+
+        public int numero { get; set; }
+
+    }
+}

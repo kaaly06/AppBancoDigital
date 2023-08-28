80c48d207a7edf52ddd82dda9a5afeb45a99a2ce
kaaly06 <kaliany.palma@etec.sp.gov.br>
2023-08-28 09:27:11 -0300

    atualização: tela de entrada - tentativa de colocar um carrossel

--- AppBancoDigital/AppBancoDigital/View/TelaEntrada.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/TelaEntrada.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +18,29 @@ namespace AppBancoDigital.View
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             //Logo do App
-            Logo.Source = ImageSource.FromResource("AppBancoDigital.Imagens.logo-remove.png");
+            Logo.Source = ImageSource.FromResource("AppBancoDigital.Imagens.logo-remove (2).png");
             //Assistente.Source = ImageSource.FromResource("AppBancoDigital.Imagens.Assistente.png");
+
+            CarouselView carouselView = new CarouselView();
+            carouselView.SetBinding(ItemsView.ItemsSourceProperty, "Assistente");
+
+           carouselView.ItemTemplate = new DataTemplate(typeof(TelaEntrada));
+            {
+                Image image = new Image {Source};
+                image.SetBinding(Image.SourceProperty, "ImgUrl");
+            }
+
+           
+           
         }
+
+        public class Assistente
+        {
+            public string ImgUrl { get; set; }
+        }
+
+
     }
+
+       
 }

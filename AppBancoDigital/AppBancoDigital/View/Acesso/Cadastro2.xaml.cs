9c43954c761acd308a3cde68dc404b16dfb84f20
Usuario <Usuario@CPD03-M01>
2023-10-03 12:19:38 -0300

    estilização: modificando todas as telasss

---
+++ AppBancoDigital/AppBancoDigital/View/Acesso/Cadastro2.xaml.cs
@@ -0,0 +1,66 @@
+﻿using AppBancoDigital.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View.Acesso
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Cadastro2 : ContentPage
+	{
+		public Cadastro2 ()
+		{
+			InitializeComponent ();
+            btn_senha.Source = ImageSource.FromResource("AppBancoDigital.Imagens.FecharOlho");
+        }
+
+        private async void btn_Cadastrar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Model.Correntista c = await DataServiceCorrentista.save(new Model.Correntista
+                {
+                    senha = txt_senha.Text,
+                    email = txt_email.Text
+
+                });
+
+                if (c.Id != null)
+                {
+                    App.DadosCorrentista = c;
+
+                    await Navigation.PushAsync(new LoginCorrentista());
+                }
+                else
+                    throw new Exception("Ocorreu um erro ao Salvar seu Cadastro.");
+                {
+
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }          
+        }
+
+        private void btn_senha_Clicked(object sender, EventArgs e)
+        {
+            if (txt_senha.IsPassword == true)
+            {
+                txt_senha.IsPassword = false;
+                btn_senha.Source = ImageSource.FromResource("AppBancoDigital.Imagens.FecharOlho.png");
+            }
+            else
+            {
+                txt_senha.IsPassword = true;
+                btn_senha.Source = ImageSource.FromResource("AppBancoDigital.Imagens.AbrirOlho.png");
+
+            }
+        }
+    }
+}
--- AppBancoDigital/AppBancoDigital/View/Acesso/CadastroCorrentista.xaml.cs
+++ AppBancoDigital/AppBancoDigital/View/Acesso/CadastroCorrentista.xaml.cs
@@ -22,7 +22,6 @@ namespace AppBancoDigital.View
 			InitializeComponent ();
 			NavigationPage.SetHasNavigationBar(this, false);
             dtcpk_data_nasc.MaximumDate = DateTime.Now.AddYears(-17);
-            btn_senha.Source = ImageSource.FromResource("AppBancoDigital.Imagens.FecharOlho");
 			btn_voltar.Source = ImageSource.FromResource("AppBancoDigital.Imagens.Voltar.png");
 
 			//var editor = new Editor { Keyboard = Keyboard.Email};
@@ -31,19 +30,17 @@ namespace AppBancoDigital.View
             editor.Keyboard = Keyboard.Create(KeyboardFlags.Suggestions | KeyboardFlags.CapitalizeWord);
         }
 
-        private async void Button_Clicked_cadastrar(object sender, EventArgs e)
-        {
-          try
+		private async void Button_Clicked_cadastrar(object sender, EventArgs e)
+		{
+			try
 			{
 				Model.Correntista c = await DataServiceCorrentista.save(new Model.Correntista
-					{
-                    nome = txt_nome.Text,
+				{
+					nome = txt_nome.Text,
 					data_nasc = dtcpk_data_nasc.Date,
-                    senha = txt_senha.Text,
-                    email = txt_email.Text,
-                    CPF = txt_cpf.Text.Replace(".", string.Empty).Replace("-", string.Empty)
+					CPF = txt_cpf.Text.Replace(".", string.Empty).Replace("-", string.Empty)
 
-                });
+				});
 
 				if (c.Id != null)
 				{
@@ -54,33 +51,21 @@ namespace AppBancoDigital.View
 				else
 					throw new Exception("Ocorreu um erro ao Salvar seu Cadastro.");
 			}
-			catch(Exception ex)
+			catch (Exception ex)
 			{
 				Console.WriteLine(ex.StackTrace);
 				await DisplayAlert("Ops!", ex.Message, "OK");
-            }
-
-			
-        }
-
-        private void btn_MostrarSenha(object sender, EventArgs e)
-        {
-			if (txt_senha.IsPassword == true)
-			{
-				txt_senha.IsPassword = false;
-				btn_senha.Source = ImageSource.FromResource("AppBancoDigital.Imagens.FecharOlho.png");
-			}
-			else
-			{
-                txt_senha.IsPassword = true;
-                btn_senha.Source = ImageSource.FromResource("AppBancoDigital.Imagens.AbrirOlho.png");
-
-            }
-        }
+			}			
+        }    
 
         private void btn_voltar_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new View.LoginCorrentista());
+            Navigation.PushAsync(new View.Acesso.Cadastro2());
+        }
+
+        private void btn_continuar_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
